ed0bd952eb8dfee4e424099558ae494010f91dab
eco-watcher <161592346+eco-watcher@users.noreply.github.com>
2024-03-06 15:39:28 +0530

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -7,18 +7,30 @@
 
 
 
-
 // PANZOOM / MAP CONTROLS:
 // ----------------------------------------------
 // (panjoom.js library by Timmy Willison and other contributors)
 // https://github.com/timmywil/panzoom/blob/main/MIT-License.txt, 
 // https://timmywil.com/panzoom
 
+
 // get the elements
 const map = document.getElementById('map');
 const zoomInButton = document.getElementById('zoomIn');
 const zoomOutButton = document.getElementById('zoomOut');
 const resetButton = document.getElementById('reset');
+
+
+// ROTATE / MAP CONTROLS:
+// ----------------------------------------------
+// (zoom library by Anita SV)
+// https://github.com/anitasv/zoom
+
+var zm = new Zoom(map, {
+  rotate: true
+});
+
+
 
 // initiate panzoom
 let panzoom = {}
@@ -36,5 +48,7 @@ zoomInButton.addEventListener('click', panzoom.zoomIn)
 zoomOutButton.addEventListener('click', panzoom.zoomOut)
 resetButton.addEventListener('click', panzoom.reset)
 map.parentElement.addEventListener('wheel', panzoom.zoomWithWheel)
+
+
 
 
